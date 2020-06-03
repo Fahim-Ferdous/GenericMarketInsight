13089dc5e3a7429d9d36312b0bb5b2b8e7ceb5be
Fahim-Ferdous <fahimferdous1312@gmail.com>
2020-06-03 19:35:53 +0600

    feat(RyansComputers): added brand field to product

--- GenericMarketInsight/spiders/RyansComputers.py
+++ GenericMarketInsight/spiders/RyansComputers.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import logging
+import string
+from time import sleep
 from urllib.parse import parse_qsl, urlencode, urlparse, urlunparse
 
 import scrapy
@@ -17,12 +20,55 @@ def update_limit_qs(link, limit=72):
     return update_url_query(link, {'limit': limit})
 
 
+def remove_puncts(text):
+    return ''.join([' ' if c in string.punctuation else c for c in text])
+
+
 class RyanscomputersSpider(scrapy.Spider):
+    brand_corrections = {
+        'A Data': 'ADATA',
+        'A4 Tech': 'A4TECH',
+    }
+
     name = 'RyansComputers'
     allowed_domains = ['ryanscomputers.com', 'www.ryanscomputers.com']
     start_urls = ['https://ryanscomputers.com']
+    brand_cache = {}
+    brand_cache_completion = 0
+
+    def populate_brand_cache(self, response, brand):
+        self.brand_cache[response.css(
+            '.product-logo img::attr(src)').get()] =\
+            self.brand_corrections.get(brand, brand)
+        self.brand_cache_completion -= 1
+        yield {
+            response.css(
+                '.product-logo img::attr(src)').get(): brand,
+        }
 
     def parse(self, response):
+        # Populate url-brand dictionary.
+        selection = response.css('#brandsModal li a')
+        self.brand_cache_completion = len(selection)
+        for brand in selection:
+            yield response.follow(
+                brand.css('::attr(href)').get().strip(),
+                self.populate_brand_cache, cb_kwargs={
+                    'brand': brand.css('::text').get().strip(),
+                })
+
+        self.log("{} BRANDS AVAILABLE".format(
+            self.brand_cache_completion), logging.INFO)
+
+        # Wait until populated
+        while self.brand_cache_completion != 0:
+            sleep(1)
+            yield
+
+        self.log("BRAND CACHE DONE ({} BRANDS)".format(
+            len(self.brand_cache)), logging.INFO)
+
+        # Item parsing and yielding starts form here.
         for item in response.css(".nav-item")[1:]:
             category = item.css('::text').get().strip()
             subcategory1 = None
@@ -50,23 +96,44 @@ class RyanscomputersSpider(scrapy.Spider):
                     }
                 )
 
+    def match_brand(self, brand_filters, title):
+        for brand_filter in brand_filters:
+            if remove_puncts(title.lower()).startswith(
+                    remove_puncts(brand_filter.lower())):
+                return brand_filter
+        self.log("NO BRANDS FOR {}".format(title), logging.WARNING)
+        return None
+
     def parse_grid(self, response, category, subcategory1, subcategory2):
         next_page = response.css("a[rel=next]::attr(href)").get()
         if next_page:
-            yield response.follow(update_limit_qs(next_page), self.parse_grid, cb_kwargs={
-                'category': category,
-                'subcategory1': subcategory1,
-                'subcategory2': subcategory2,
-            })
+            yield response.follow(
+                update_limit_qs(next_page), self.parse_grid, cb_kwargs={
+                    'category': category,
+                    'subcategory1': subcategory1,
+                    'subcategory2': subcategory2,
+                })
 
-        for link in response.css(".product-title-grid::attr(href)").extract():
-            yield response.follow(link, self.parse_product, cb_kwargs={
-                'category': category,
-                'subcategory1': subcategory1,
-                'subcategory2': subcategory2,
-            })
+        brand_filters = [brand.strip() for brand in response.css(
+            '.default-brand-filters button::text').extract()]
 
-    def parse_product(self, response, category, subcategory1, subcategory2):
+        brand_filters.extend(self.brand_cache.values())
+
+        for box in response.css(".product-box"):
+            cache_hit = self.brand_cache.get(
+                box.css('.product-logo img::attr(src)').get())
+            yield response.follow(
+                box.css('.product-title-grid::attr(href)').get(),
+                self.parse_product, cb_kwargs={
+                    'category': category,
+                    'subcategory1': subcategory1,
+                    'subcategory2': subcategory2,
+                    'brand': cache_hit if cache_hit else self.match_brand(
+                        brand_filters,
+                        box.css('.product-title-grid::text').get()),
+                })
+
+    def parse_product(self, response, category, subcategory1, subcategory2, brand):
         details = response.css('.produc-details-short')
         reviews = []
         for comment in response.css('.comments'):
@@ -87,6 +154,7 @@ class RyanscomputersSpider(scrapy.Spider):
             'category': category,
             'subcategory1': subcategory1,
             'subcategory2': subcategory2,
+            'brand': brand,
             'productId': details.css("p > span::text").get().strip(),
             'priceOld': (details.css('.old-price::text').get() or '0').strip(),
             'price': (details.css('.price::text').get() or '0').strip(),
